26527e1d6af7e3ce12432ad05127da597879cfa0
Quang Trung <quangtrungit1106@gmail.com>
2025-08-08 10:49:55 +0700

    fix

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
 import type { QuestionData } from "../types/question";
 import {
